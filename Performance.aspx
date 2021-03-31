4aee39d04011f6b6d904c5f37f01f82fcb4a988b
Henrique <ceuhenrique@hotmail.com>
2021-03-31 20:13:29 +0100

    Diagram performance updated with nodes and connections

--- Performance.aspx
+++ Performance.aspx
@@ -2,54 +2,34 @@
 <%@ Register TagPrefix="telerik" Namespace="Telerik.Web.UI" Assembly="Telerik.Web.UI" %>
 
 
+
 <asp:Content ID="Content0" ContentPlaceHolderID="head" Runat="Server">
+   
+
+
     <link href="assets/styles/base.css" rel="stylesheet" />
     <link href="assets/styles/default.css" rel="stylesheet" />
     <script src="assets/scripts/functions.js"></script> 
+   <script type="text/javascript">
+
+     
+       $(document).ready(function () {
+
+           $("#width").val() = $(window).width();
+           $("#height").val() = $(window).height();
+
+       });
+   </script>
 </asp:Content>
 
  
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+
+    <asp:HiddenField ID="width" runat="server" />
+    <asp:HiddenField ID="height" runat="server" />
+
     <div>
-
-        <asp:Label ID="APIResult" Text="" runat="server"></asp:Label>
-             
-        <telerik:RadDiagram ID="RadDiagram" runat="server" Selectable="false" Editable="false">
-            <ShapesCollection>
-                <telerik:DiagramShape Id="DiagramShape1" Width="100" Height="70" X="260" Y="100" Type="rectangle">
-                    <ContentSettings Text="Parent" />
-                    <FillSettings Color="#25a0da" />
-                </telerik:DiagramShape>
-                <telerik:DiagramShape Id="DiagramShape2" Height="70" X="60" Y="250" Type="circle">
-                    <ContentSettings Text="Child 1" />
-                    <FillSettings Color="#FFBE33" />
-                </telerik:DiagramShape>
-                <telerik:DiagramShape Id="DiagramShape3" Height="70" X="245" Y="250" Type="circle">
-                    <ContentSettings Text="Child 2" />
-                    <FillSettings Color="#FFBE33" />
-                </telerik:DiagramShape>
-                <telerik:DiagramShape Id="DiagramShape4" Height="70" X="440" Y="250" Type="circle">
-                    <ContentSettings Text="Child 3" />
-                    <FillSettings Color="#FFBE33" />
-                </telerik:DiagramShape>
-            </ShapesCollection>
-
-            <ConnectionsCollection>
-                <telerik:DiagramConnection StartCap="FilledCircle" EndCap="ArrowEnd">
-                    <FromSettings Connector="Bottom" ShapeId="DiagramShape1" />
-                    <ToSettings Connector="Top" ShapeId="DiagramShape2" />
-                </telerik:DiagramConnection>
-
-                <telerik:DiagramConnection StartCap="FilledCircle" EndCap="ArrowEnd">
-                    <FromSettings Connector="Bottom" ShapeId="DiagramShape1" />
-                    <ToSettings Connector="Top" ShapeId="DiagramShape3" />
-                </telerik:DiagramConnection>
-
-                 <telerik:DiagramConnection StartCap="FilledCircle" EndCap="ArrowEnd">
-                    <FromSettings Connector="Bottom" ShapeId="DiagramShape1" />
-                    <ToSettings Connector="Top" ShapeId="DiagramShape4" />
-                </telerik:DiagramConnection>    </ConnectionsCollection>
-        </telerik:RadDiagram>
-
+        <asp:Label ID="APIResult" Text="" runat="server"></asp:Label>           
     </div>
+
 </asp:Content>
--- Performance.aspx.cs
+++ Performance.aspx.cs
@@ -9,13 +9,62 @@ using System.Net.Http.Headers;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Web.UI;
 
 public partial class Performance : System.Web.UI.Page
 {
+
+    RadDiagram RadDiagram1 = new RadDiagram();
+
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        var TheBrowserWidth = width.Value;
+        var TheBrowserHeight = height.Value;
+
         callWorkflows();
+      
+
+        // General diagram settings
+        RadDiagram1.Width = new System.Web.UI.WebControls.Unit(width.Value); 
+        RadDiagram1.Height = new System.Web.UI.WebControls.Unit(height.Value);
+
+        RadDiagram1.ShapeDefaultsSettings.Width = 140;
+        RadDiagram1.ShapeDefaultsSettings.Height = 30;
+        RadDiagram1.ShapeDefaultsSettings.StrokeSettings.Color = "#fff";
+        Form.Controls.Add(RadDiagram1);
+
+        // Layout settings
+        RadDiagram1.LayoutSettings.Enabled = true;
+        RadDiagram1.LayoutSettings.Type = Telerik.Web.UI.Diagram.LayoutType.Layered;
+        RadDiagram1.LayoutSettings.Subtype = Telerik.Web.UI.Diagram.LayoutSubtype.Right;
+        RadDiagram1.LayoutSettings.VerticalSeparation = 20;
+        RadDiagram1.LayoutSettings.HorizontalSeparation = 30;
+
+       
+      
     }
+
+    protected void AddDiagramShape(string shapeID, string contentText, RadDiagram diagram)
+    {
+        var shape = new DiagramShape()
+        {
+            Id = shapeID,
+        };
+        shape.ContentSettings.Text = contentText;
+        //shape.ContentSettings.Color = contentColor;
+        //shape.FillSettings.Color = backgroundColor;
+        diagram.ShapesCollection.Add(shape);
+    }
+
+    protected void ConnectDiagramShapes(string startShapeID, string endShapeID, RadDiagram diagram)
+    {
+        var connection = new DiagramConnection();
+        connection.FromSettings.ShapeId = startShapeID;
+        connection.ToSettings.ShapeId = endShapeID;
+        diagram.ConnectionsCollection.Add(connection);
+    }
+
 
     protected async void callWorkflows()
     {
@@ -33,14 +82,64 @@ public partial class Performance : System.Web.UI.Page
                 if (status == true)
                 {
                     string apiResponse = await response.Content.ReadAsStringAsync();
-                    Debug.WriteLine(apiResponse);
-                    APIResult.Text = apiResponse;
+
+                    JObject jsonResponse = JObject.Parse(apiResponse);
+
+                    var nodes = jsonResponse["nodes"];
+
+                    var relations = jsonResponse["relations"];
+
+                    APIResult.Text = relations.ToString();                 
+
+                    foreach (var node in nodes.Select((value, i) => new { i, value }))
+                    {
+                        AddDiagramShape(node.i + "", node.value + " - " + node.i, RadDiagram1);                       
+                                                               
+                    }
+
+                    foreach (var relation in relations.Select((value, i) => new { i, value }))
+                    {
+                                                                  
+                        foreach (var destination in relation.value["to"].Select((value, i) => new { i, value }))
+                        {                           
+                            Debug.WriteLine(destination.value);
+                            ConnectDiagramShapes(relation.value["from"].ToString(), destination.value.ToString(), RadDiagram1);
+                        }                       
+                    }
                 }
                 else
                 {
                     Debug.WriteLine("Something went bad.");
+
+                  
                 }
             }
         }
     }
+
+    //public static class Nodes
+    //{
+    //    public static Node Nadal = new Node() { Name = "R. Nadal", Id = "nadal", Color = "#f18100" };
+    //    public static Node Djokovic = new Node() { Name = "N. Djokovic", Id = "djokovic", Color = "#8cb20f" };
+    //    public static Node Youzhny = new Node() { Name = "M. Youzhny", Id = "youzhny", Color = "#ae5e08" };
+    //    public static Node Murray = new Node() { Name = "A. Murray", Id = "murray", Color = "#d75234" };
+    //    public static Node Wawrinka = new Node() { Name = "S. Wawrinka", Id = "wawrinka", Color = "#f8c43a" };
+    //    public static Node Gasquet = new Node() { Name = "R. Gasquet", Id = "gasquet", Color = "#5f9fee" };
+    //    public static Node Ferrer = new Node() { Name = "D. Ferrer", Id = "ferrer", Color = "#1958a6" };
+    //    public static Node Robredo = new Node() { Name = "T. Robredo", Id = "Robredo", Color = "#6da000" };
+    //}
+
+    //public class Workflow
+    //{
+    //    public string Name { get; set; }
+
+    //    public IList<Node> Node { get; set; }
+    //}
+
+    //public class Node
+    //{
+    //    public string Name { get; set; }
+    //    public string Id { get; set; }
+    //    public string Color { get; set; }
+    //}
 }
